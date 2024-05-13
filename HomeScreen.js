c4ba31fd7dda4803f65d3320b13d1b1ee789922e
MrTex09 <bagente24@gmail.com>
2024-05-13 10:58:32 -0300

    se agrego estilos

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button,StyleSheet } from 'react-native';
 
 // Definición del componente HomeScreen
 export default function HomeScreen({ navigation }) {
@@ -11,20 +11,44 @@ export default function HomeScreen({ navigation }) {
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <View>     
-        <Text>Visitas a tus amigos: {visits}</Text>
-        <View>
+        <Text style={styles.title}>Visitas a tus amigos: {visits}</Text>
+        <View style={styles.buttonContainer}>
           <Button
             title="Ve a ver tus amigos"
             onPress={handlePress}
+            color={styles.friendsButton.color}
           />
           <Button 
             title="Ver personajes de Marvel"
             onPress={() => navigation.navigate('Marvel Characters')}
+            color={styles.marvelButton.color} 
           />
         </View>
       </View>
     </View>
   );
 }
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  justifyContent: 'space-between',
+},
+title: {
+  fontSize: 22,
+  fontWeight: 'bold',
+  marginBottom: 20,
+},
+buttonContainer: {
+  flexDirection: 'row', // Ajusta los botones horizontalmente
+  justifyContent: 'space-around', // Espaciado entre botones
+  width: '100%', // Asegura que el contenedor utilice todo el ancho disponible
+  paddingHorizontal: 0, // Añade algo de espacio horizontalmente
+},
+friendsButton: {
+  color: "blue", // Color para el botón de amigos
+},
+marvelButton: {
+  color: 'red', // Color para el botón de Marvel, se cambió backgroundColor por color
+},});
